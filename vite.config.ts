9f0c5273b6dc14d9f2e7c2ab93915e0e670cc94c
Cho Si Yeon <chosiyeonn@gmail.com>
2024-01-30 00:44:29 +0900

    fix: sentry에만 sourcemap 주입

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "fe-error-logging",
-    project: "javascript-react2"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "fe-error-logging",
+      project: "javascript-react2",
+      sourcemaps: {
+        filesToDeleteAfterUpload: "./dist/**/*.map",
+      },
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
