de9c9cd5b8a24c7ba00f33f95ce74296d3c7b8c8
Cho Si Yeon <chosiyeonn@gmail.com>
2024-01-30 01:31:22 +0900

    test: sourcemap 유출 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    sentryVitePlugin({
-      org: "fe-error-logging",
-      project: "javascript-react2",
-      sourcemaps: {
-        filesToDeleteAfterUpload: "./dist/**/*.map",
-      },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-  build: {
-    sourcemap: true,
-  },
+  return {
+    plugins: [
+      react(),
+      sentryVitePlugin({
+        org: "fe-error-logging",
+        project: "javascript-react2",
+        authToken: env.VITE_SENTRY_AUTH_TOKEN,
+        sourcemaps: {
+          filesToDeleteAfterUpload: "./dist/**/*.map",
+        },
+      }),
+    ],
+
+    build: {
+      sourcemap: true,
+    },
+  };
 });
